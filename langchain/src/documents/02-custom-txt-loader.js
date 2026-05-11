3ddce4438a6b21e14fedc24221e67b23ae20f07a
Farhan Raza <farhanraza2239@gmail.com>
2026-05-11 09:55:21 +0530

    extract meta data dynimically

--- langchain/src/documents/02-custom-txt-loader.js
+++ langchain/src/documents/02-custom-txt-loader.js
@@ -3,27 +3,27 @@ import path from "path";
 import { Document } from "@langchain/core/documents";
 import { createCipheriv } from "crypto";
 
-class CustomTxtLoader {
-  constructor(filePath, metadata = {}) {
-    this.filePath = filePath;
-    this.metadata = metadata;
-  }
+// class CustomTxtLoader {
+//   constructor(filePath, metadata = {}) {
+//     this.filePath = filePath;
+//     this.metadata = metadata;
+//   }
 
-  async load() {
-    const absolutePath = path.resolve(this.filePath);
-    const text = await fs.readFile(absolutePath, "utf-8");
+//   async load() {
+//     const absolutePath = path.resolve(this.filePath);
+//     const text = await fs.readFile(absolutePath, "utf-8");
 
-    return [
-      new Document({
-        pageContent: text,
-        metadata: {
-          ...this.metadata,
-          source: this.filePath,
-        },
-      }),
-    ];
-  }
-}
+//     return [
+//       new Document({
+//         pageContent: text,
+//         metadata: {
+//           ...this.metadata,
+//           source: this.filePath,
+//         },
+//       }),
+//     ];
+//   }
+// }
 
 // const loader = new CustomTxtLoader("data/class-10/science/life-processes.txt", {
 //   classLevel: "10",
@@ -43,19 +43,69 @@ class CustomTxtLoader {
 // console.log("\n--- DOCUMENT 1 METADATA ---");
 // console.log(docs[0].metadata);
 
-const loader = new CustomTxtLoader("data/cricketer/virat_kohli/01-virat-kohli.txt", {
-  cricketer: "virat kohli",
-  subject: "about-virat-kohli",
+// const loader = new CustomTxtLoader("data/cricketer/virat_kohli/01-virat-kohli.txt", {
+//   cricketer: "virat kohli",
+//   subject: "about-virat-kohli",
   
-});
+// });
+
+// const docs = await loader.load();
+
+// console.log("\n--- TOTAL DOCUMENTS ---");
+// console.log(docs.length);
+
+// console.log("\n--- DOCUMENT 1 PAGE CONTENT ---");
+// console.log(docs[0].pageContent);
+
+// console.log("\n--- DOCUMENT 1 METADATA ---");
+// console.log(docs[0].metadata);
+
+
+function extractMetadataFromPath(filePath) {
+  const normalizedPath = filePath.replaceAll("\\", "/");
+  const parts = normalizedPath.split("/");
+
+  const fileName = parts.at(-1);
+  const chapter = fileName.replace(".txt", "");
+
+  return {
+    classLevel: parts[1]?.replace("class-", ""),
+    subject: parts[2],
+    chapter,
+    board: "bihar-board",
+    source: normalizedPath,
+  };
+}
+
+class CustomTxtLoader {
+  constructor(filePath) {
+    this.filePath = filePath;
+  }
+
+  async load() {
+    const absolutePath = path.resolve(this.filePath);
+    const text = await fs.readFile(absolutePath, "utf-8");
+
+    const metadata = extractMetadataFromPath(this.filePath);
+
+    return [
+      new Document({
+        pageContent: text,
+        metadata,
+      }),
+    ];
+  }
+}
+
+const loader = new CustomTxtLoader("data/class-10/science/life-processes.txt");
 
 const docs = await loader.load();
 
 console.log("\n--- TOTAL DOCUMENTS ---");
 console.log(docs.length);
 
-console.log("\n--- DOCUMENT 1 PAGE CONTENT ---");
-console.log(docs[0].pageContent);
+console.log("\n--- PAGE CONTENT PREVIEW ---");
+console.log(docs[0].pageContent.slice(0, 120));
 
-console.log("\n--- DOCUMENT 1 METADATA ---");
+console.log("\n--- AUTO METADATA ---");
 console.log(docs[0].metadata);
